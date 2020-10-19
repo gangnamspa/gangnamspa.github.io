5636c8d94e9baeae8b453e52c9b041d11e4d9e8d
Jimmy Hong <jimmyhong.rocks@gmail.com>
2020-10-19 14:32:00 -0500

    Updates

---
+++ precache-manifest.54d32049e31e1db06b0ad919554f14df.js
@@ -0,0 +1,146 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dba7047ed82e75289741660d07c513c7",
+    "url": "/index.html"
+  },
+  {
+    "revision": "34158010208b709da107",
+    "url": "/static/css/main.0d003ca3.chunk.css"
+  },
+  {
+    "revision": "f363a29a31538486b760",
+    "url": "/static/js/2.63e3aa23.chunk.js"
+  },
+  {
+    "revision": "d916bdeee074e24b650042bd2a3d71cf",
+    "url": "/static/js/2.63e3aa23.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "34158010208b709da107",
+    "url": "/static/js/main.c1a70fd1.chunk.js"
+  },
+  {
+    "revision": "79798cdf5221996f9ada",
+    "url": "/static/js/runtime-main.2fad0d7b.js"
+  },
+  {
+    "revision": "5802af1dbb775c49129fdd1a152e0f75",
+    "url": "/static/media/1938.5802af1d.jpg"
+  },
+  {
+    "revision": "caf3f077fd3bc4dd5164a1a4e3c03610",
+    "url": "/static/media/1968.caf3f077.jpg"
+  },
+  {
+    "revision": "821261853413c21337be51b5914cdfb4",
+    "url": "/static/media/2001.82126185.jpg"
+  },
+  {
+    "revision": "b2179bff376e618fe1380b6a35f81ef0",
+    "url": "/static/media/2029.b2179bff.jpg"
+  },
+  {
+    "revision": "47231dfd457d98ba017c625b602671b6",
+    "url": "/static/media/2034.47231dfd.jpg"
+  },
+  {
+    "revision": "661408be4c203a5c3174c0c8ceb7efc2",
+    "url": "/static/media/2044.661408be.jpg"
+  },
+  {
+    "revision": "d3ad0ecad661d3aa38547c95669005c5",
+    "url": "/static/media/4324.d3ad0eca.jpg"
+  },
+  {
+    "revision": "3e4aef722c8c5192287c121908d8956d",
+    "url": "/static/media/4329.3e4aef72.jpg"
+  },
+  {
+    "revision": "5d9f4fb5c329598bc5aa775907f32eb7",
+    "url": "/static/media/4380.5d9f4fb5.jpg"
+  },
+  {
+    "revision": "d8f3b64a95ea60e46357d918b7b4d5f5",
+    "url": "/static/media/4389.d8f3b64a.jpg"
+  },
+  {
+    "revision": "e92b4028cbc611c526b9db136258a43e",
+    "url": "/static/media/4413.e92b4028.jpg"
+  },
+  {
+    "revision": "6761124272324b203021cff4ef18494d",
+    "url": "/static/media/4419.67611242.jpg"
+  },
+  {
+    "revision": "2b3a4c547dac23a23ae895e542389f13",
+    "url": "/static/media/4425.2b3a4c54.jpg"
+  },
+  {
+    "revision": "f96381af1f09eb06fadf7a34c221043c",
+    "url": "/static/media/4467.f96381af.jpg"
+  },
+  {
+    "revision": "7b909366f0605d0e0a406ac5ac102d9a",
+    "url": "/static/media/4506.7b909366.jpg"
+  },
+  {
+    "revision": "0f719f74746d74ad86e5abe831e8011c",
+    "url": "/static/media/application-form.0f719f74.pdf"
+  },
+  {
+    "revision": "358037af256c8b27b6abdf4fe2822b91",
+    "url": "/static/media/chicken.358037af.jpg"
+  },
+  {
+    "revision": "ee54a7010b367944616a98ec2a09511b",
+    "url": "/static/media/coldnoodle.ee54a701.jpg"
+  },
+  {
+    "revision": "b8fe09c3420c3c3fa7d611801f8dfcd8",
+    "url": "/static/media/inada.b8fe09c3.jpg"
+  },
+  {
+    "revision": "4437bcc123045718ed26e78b4bab4f3b",
+    "url": "/static/media/kimbop.4437bcc1.jpg"
+  },
+  {
+    "revision": "acc66f9534efbd98142fa5d3141d7307",
+    "url": "/static/media/logo.acc66f95.svg"
+  },
+  {
+    "revision": "7782b05a56b457950e10d6f928c1c897",
+    "url": "/static/media/membership-cards-gold.7782b05a.jpg"
+  },
+  {
+    "revision": "975df0e9fa55ff1f7cf5d36632a8172e",
+    "url": "/static/media/membership-cards-platinum.975df0e9.jpg"
+  },
+  {
+    "revision": "8eeb68599d049cbc16e54a26245c3f8f",
+    "url": "/static/media/membership-cards-silver.8eeb6859.jpg"
+  },
+  {
+    "revision": "f4f871fce31836a1566716316b2ea741",
+    "url": "/static/media/nail-shop.f4f871fc.jpg"
+  },
+  {
+    "revision": "9eef974113057fd01b2d5802f3a83315",
+    "url": "/static/media/prohibited_items.9eef9741.png"
+  },
+  {
+    "revision": "c22a5d50cbf758f57a0b7a90a3436f10",
+    "url": "/static/media/service2.c22a5d50.jpg"
+  },
+  {
+    "revision": "1c8c0c65e8fc94c861a48f679448e542",
+    "url": "/static/media/service3.1c8c0c65.jpg"
+  },
+  {
+    "revision": "bdb319a09862c174c49e1a2710c3a520",
+    "url": "/static/media/shaved_ice.bdb319a0.jpg"
+  },
+  {
+    "revision": "ed40daa6b58e11a1e025dc80f25a0d46",
+    "url": "/static/media/spa_etiquette.ed40daa6.png"
+  }
+]);
