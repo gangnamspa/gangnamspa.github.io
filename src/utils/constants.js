c15d15697bf1bffca23baaab3be748034374b1f5
Jimmy Hong <jimmy@foreflight.com>
2021-01-15 09:02:05 +0900

    fix phone number

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,7 +1,7 @@
 export const currentDate = new Date();
 
 export const COMPANY_NAME = "GangNam Spa";
-export const PHONE_NUMBERS = "2818599889";
+export const PHONE_NUMBERS = "2818599888";
 export const PHONE = "281-859-9888";
 export const PHONE_FORMATTED = "281 - 859 - 9888";
 export const STREET = "4055 Hwy 6 North";
@@ -11,7 +11,7 @@ export const DINING_HOURS = `Sun-Thu: 10am - 8:30pm\n Fri-Sat: 10am - 9pm`;
 export const EMAIL = "spagangnam@gmail.com";
 export const PRICE = {
   DAILY: "$39",
-  WEEKENDS: "$49",
+  WEEKENDS: "$49"
 };
 export const PROMOTION_BANNER = `On December 24th & 31st, GangNam Spa will be open from 10am and closing at 6pm.\n Happy holidays!`;
 export const CURRENT_PROMOTION = "Admission Membership Promotion";
@@ -29,338 +29,338 @@ export const DINING_MENU = {
     {
       title: "Spicy Korean Chicken Stew",
       korean: "닭도리탕",
-      price: 14.99,
+      price: 14.99
     },
     {
       title: "Seaweed Soup",
       korean: "미역국",
-      price: 12.99,
+      price: 12.99
     },
     {
       title: "Cabbage Soup",
       korean: "우거지해장국",
-      price: 12.99,
+      price: 12.99
     },
     {
       title: "Spicy Beef Soup with Vegetables",
       korean: "육계장",
-      price: 14.99,
+      price: 14.99
     },
     {
       title: "Ox Bone Soup",
       korean: "설렁탕",
-      price: 14.99,
+      price: 14.99
     },
     {
       title: "Dumpling Soup",
       korean: "떡만두국",
-      price: 11.99,
+      price: 11.99
     },
     {
       title: "Tofu Soup",
       korean: "순두부찌개",
-      price: 12.99,
-    },
+      price: 12.99
+    }
   ],
   vegetables: [
     {
       title: "Stir Fried Vegetables",
       korean: "야채볶음",
-      price: 12.99,
+      price: 12.99
     },
     {
       title: "Vegetable Fried Rice",
       korean: "야채 볶음밥",
-      price: 13.99,
+      price: 13.99
     },
     {
       title: "Vegetable Udon",
       korean: "야채우동",
-      price: 12.99,
+      price: 12.99
     },
     {
       title: "Vegetable Noodles",
       korean: "야채칼국수",
-      price: 12.99,
-    },
+      price: 12.99
+    }
   ],
   rice: [
     {
       title: "Bibimbap",
       korean: "비빔밥",
-      price: 12.99,
+      price: 12.99
     },
     {
       title: "Dolsot Bibimbap",
       korean: "돌솥 비빔밥",
-      price: 15.99,
+      price: 15.99
     },
     {
       title: "Shrimp Fried Rice",
       korean: "새우 볶음밥",
-      price: 13.99,
+      price: 13.99
     },
     {
       title: "Chicken Fried Rice",
       korean: "닭 볶음밥",
-      price: 13.99,
+      price: 13.99
     },
     {
       title: "Omurice",
       korean: "오므라이스",
-      price: 12.99,
+      price: 12.99
     },
     {
       title: "Rice with Beef",
       korean: "불고기덮밥",
-      price: 16.99,
+      price: 16.99
     },
     {
       title: "Rice with Pork",
       korean: "돼지불고기덮밥",
-      price: 14.99,
+      price: 14.99
     },
     {
       title: "Rice with Chicken",
       korean: "닭불고기덮밥",
-      price: 12.99,
+      price: 12.99
     },
     {
       title: "Rice with Japchae",
       korean: "잡채덮밥",
-      price: 12.99,
-    },
+      price: 12.99
+    }
   ],
   noodles: [
     {
       title: "Seafood Udon",
       korean: "해물우동",
-      price: 13.99,
+      price: 13.99
     },
     {
       title: "Spicy Beef Noodles",
       korean: "육계장칼국수",
-      price: 14.99,
+      price: 14.99
     },
     {
       title: "JjamPpong Noodles",
       korean: "짬뽕칼국수",
-      price: 14.99,
+      price: 14.99
     },
     {
       title: "Cold Noodles",
       korean: "물냉면",
-      price: 12.99,
+      price: 12.99
     },
     {
       title: "Spicy Cold Noodles",
       korean: "비빔냉면",
-      price: 12.99,
-    },
+      price: 12.99
+    }
   ],
   meat: [
     {
       title: "Stir Fried Spicy Pork BBQ",
       korean: "제육볶음",
-      price: 14.99,
+      price: 14.99
     },
     {
       title: "Pork Belly BBQ",
       korean: "통생삽겹살",
-      price: 24.99,
+      price: 24.99
     },
     {
       title: "Bulgogi",
       korean: "불고기",
-      price: 18.99,
+      price: 18.99
     },
     {
       title: "Pork Ribs",
       korean: "돼지갈비",
-      price: 18.99,
+      price: 18.99
     },
     {
       title: "Pork Cutlet",
       korean: "돈까스",
-      price: 14.99,
+      price: 14.99
     },
     {
       title: "Grilled Mackerel",
       korean: "고등어/이면수구이",
-      price: 12.99,
+      price: 12.99
     },
     {
       title: "Stir Fried Rice Cake",
       korean: "떡볶이",
-      price: 11.99,
+      price: 11.99
     },
     {
       title: "Seafood/Kimchi Pancake",
       korean: "해물/김치파전",
-      price: 10.99,
-    },
+      price: 10.99
+    }
   ],
   chicken: [
     {
       title: "Chicken with Fries Combo",
       korean: "치킨 감자튀김",
-      price: 12.99,
+      price: 12.99
     },
     {
       title: "Chicken Wings 8pc",
       korean: "치킨윙",
-      price: 14.99,
+      price: 14.99
     },
     {
       title: "Chicken Wings 16pc",
       korean: "치킨윙",
-      price: 25.99,
+      price: 25.99
     },
     {
       title: "Seasoned Chicken",
       korean: "닭강정",
-      price: 14.99,
+      price: 14.99
     },
     {
       title: "Gang Nam Style Chicken & Beer Set 1",
       korean: "치맥",
-      price: 24.99,
+      price: 24.99
     },
     {
       title: "Gang Nam Style Chicken & Beer Set 2",
       korean: "치맥",
-      price: 39.99,
-    },
+      price: 39.99
+    }
   ],
   fried: [
     {
       title: "Chicken Nugget with Fries",
       korean: "치킨너겟과 감자튀김",
-      price: 12.99,
+      price: 12.99
     },
     {
       title: "Fries",
       korean: "감자튀김",
-      price: 5.99,
+      price: 5.99
     },
     {
       title: "Cheese Sticks 4pc",
       korean: "치즈스틱",
-      price: 9.99,
+      price: 9.99
     },
     {
       title: "Egg Roll 3pc",
       korean: "춘권",
-      price: 7.99,
+      price: 7.99
     },
     {
       title: "Fried Dumpling",
       korean: "만두튀김",
-      price: 10.99,
+      price: 10.99
     },
     {
       title: "Egg Roll, Fries, Cheese Combo",
       korean: "춘권, 감자튀김, 치즈",
-      price: 14.99,
-    },
+      price: 14.99
+    }
   ],
   beverage: [
     {
       title: "Water Bottle",
       korean: "",
-      price: 2.5,
+      price: 2.5
     },
     {
       title: "Smart Water",
       korean: "",
-      price: 3.99,
+      price: 3.99
     },
     {
       title: "Fiji",
       korean: "",
-      price: 3.99,
+      price: 3.99
     },
     {
       title: "Pellegrino",
       korean: "",
-      price: 3.99,
+      price: 3.99
     },
     {
       title: "Vitamin Water",
       korean: "",
-      price: 3.99,
+      price: 3.99
     },
     {
       title: "Powerade",
       korean: "",
-      price: 3.99,
+      price: 3.99
     },
     {
       title: "Unsweetened/Sweet Tea",
       korean: "",
-      price: 3.99,
+      price: 3.99
     },
     {
       title: "Lipton Green Tea",
       korean: "",
-      price: 3.99,
+      price: 3.99
     },
     {
       title: "Lemonade",
       korean: "",
-      price: 3.99,
+      price: 3.99
     },
     {
       title: "Hot Coffee",
       korean: "",
-      price: 3.0,
+      price: 3.0
     },
     {
       title: "Hot Tea",
       korean: "",
-      price: 2.99,
+      price: 2.99
     },
     {
       title: "Soda Bottle",
       korean: "",
-      price: 2.99,
+      price: 2.99
     },
     {
       title: "Aloe Vera",
       korean: "알로에",
-      price: 3.99,
+      price: 3.99
     },
     {
       title: "Barley Tea",
       korean: "보리차",
-      price: 3.99,
-    },
+      price: 3.99
+    }
   ],
   bar: [
     {
       title: "Beer Bottle",
       korean: "병맥주",
-      price: 4.5,
+      price: 4.5
     },
     {
       title: "Draft Beer 16oz",
       korean: "생맥주",
-      price: 5.99,
+      price: 5.99
     },
     {
       title: "Pitcher",
       korean: "피처",
-      price: 18.99,
+      price: 18.99
     },
     {
       title: "Rice Wine",
       korean: "국순당막걸리",
-      price: 13.0,
+      price: 13.0
     },
     {
       title: "Wine Bottle",
       korean: "와인",
-      price: 39.99,
-    },
-  ],
+      price: 39.99
+    }
+  ]
 };
